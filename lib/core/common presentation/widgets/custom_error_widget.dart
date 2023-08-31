c940b61e18f84b6bdde58cff75aa2d98ec1f6d22
YousabSefeen <jesusyousab@gmail.com>
2023-08-31 17:27:04 +0200

    Reset text direction when errors occurs

--- lib/core/common presentation/widgets/custom_error_widget.dart
+++ lib/core/common presentation/widgets/custom_error_widget.dart
@@ -18,60 +18,63 @@ class CustomErrorWidget extends StatefulWidget {
 class _CustomErrorWidgetState extends State<CustomErrorWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      margin: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20.r),
-        color: const Color(0xffE0B1AA),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextButton.icon(
-            onPressed: null,
-            icon: Icon(
-              Icons.error,
-              size: 35.sp,
-              color: Colors.red,
-            ),
-            label: Text(
-              'An error occurred while displaying ${widget.errorCategoryName}',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 17.sp,
-                fontWeight: FontWeight.w800,
-                letterSpacing: 1.2,
+    return Directionality(
+      textDirection:TextDirection.ltr ,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        margin: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.r),
+          color: const Color(0xffE0B1AA),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton.icon(
+              onPressed: null,
+              icon: Icon(
+                Icons.error,
+                size: 35.sp,
+                color: Colors.red,
               ),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          const Divider(thickness: 2, color: Colors.black54),
-          RichText(
-            text: TextSpan(
-                text: 'Error Message: ',
+              label: Text(
+                'An error occurred while displaying ${widget.errorCategoryName}',
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 16.sp,
+                  fontSize: 17.sp,
                   fontWeight: FontWeight.w800,
                   letterSpacing: 1.2,
                 ),
-                children: [
-                  TextSpan(
-                    text: widget.errorMessage,
-                    style: TextStyle(
-                      color: Colors.blueGrey.shade900,
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w500,
-                      height: 1.4,
-                    ),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            const Divider(thickness: 2, color: Colors.black54),
+            RichText(
+              text: TextSpan(
+                  text: 'Error Message: ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w800,
+                    letterSpacing: 1.2,
                   ),
-                ]),
-          ),
-        ],
+                  children: [
+                    TextSpan(
+                      text: widget.errorMessage,
+                      style: TextStyle(
+                        color: Colors.blueGrey.shade900,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        height: 1.4,
+                      ),
+                    ),
+                  ]),
+            ),
+          ],
+        ),
       ),
     );
   }
